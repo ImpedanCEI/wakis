46bee0e101863fd35c39447fa9750d5fafd27b02
ELENA DE LA FUENTE <elena.delafuenteg@gmail.com>
2024-02-28 17:58:49 +0100

    add flags + field_on_stl 3d plot

--- examples/script_cubcavity_fit.py
+++ examples/script_cubcavity_fit.py
@@ -5,6 +5,7 @@ import pyvista as pv
 import h5py
 from tqdm import tqdm
 from scipy.constants import c as c_light
+from scipy.constants import epsilon_0
 
 sys.path.append('../')
 
@@ -58,12 +59,12 @@ ixt, iyt = np.abs(x-xt).argmin(), np.abs(y-yt).argmin()
 
 def beam(solver, t):
     # Define gaussian
-    s0 = z.min() - c_light*tinj
+    s0 = z.min()-c_light*tinj
     s = z-c_light*t
     profile = 1/np.sqrt(2*np.pi*sigmaz**2)*np.exp(-(s-s0)**2/(2*sigmaz**2))
 
     # Update current
-    current = q*c_light*beta*profile/solver.dx/solver.dy
+    current = q*c_light*profile/solver.dx/solver.dy
     solver.J[ixs,iys,:,'z'] = current
 
 # ------------ Time loop ----------------
@@ -76,12 +77,14 @@ Nt = int(tmax/solver.dt)
 t = np.arange(0, Nt*solver.dt, solver.dt)
 ninj = int(tinj/solver.dt)
 
-# Prepare save file
-hf = h5py.File('hdf5/Ez_abc2.h5', 'w')
-hf2 = h5py.File('hdf5/Jz_abc2.h5', 'w')
-hf['x'], hf['y'], hf['z'], hf['t'] = x, y, z, t
+# Prepare save files
+save = False
+plot = False
 
-plot = True
+if save:
+    hf = h5py.File('hdf5/Ez_abc2.h5', 'w')
+    hf2 = h5py.File('hdf5/Jz_abc2.h5', 'w')
+    hf['x'], hf['y'], hf['z'], hf['t'] = x, y, z, t
 
 for n in tqdm(range(Nt)):
 
@@ -93,21 +96,29 @@ for n in tqdm(range(Nt)):
 
     # Plot 2D
     if n>ninj and n%10 == 0 and plot:
-        solver.plot2D(field='E', component='z', plane='ZY', pos=0.5, norm='linear', 
-                      vmin=-1e5, vmax=1e5, cmap='rainbow', patch_alpha=0.5, 
-                      add_patch='Cavity', patch_reverse=True, title='imgCav/Ez', off_screen=True,  
+        solver.plot2D(field='E', component='y', plane='ZY', pos=0.5, norm=None, 
+                      vmin=-5e5, vmax=5e5, cmap='rainbow', patch_alpha=0.5, 
+                      add_patch='Cavity', patch_reverse=True, title='imgCav/Ey', off_screen=True,  
                       n=n, interpolation='spline36')
     
     # Save
-    hf['#'+str(n).zfill(5)]=solver.E[ixt, iyt, :, 'z'] 
-    hf2['#'+str(n).zfill(5)]=solver.J[ixt, iyt, :, 'z'] 
+    if save:
+        hf['#'+str(n).zfill(5)]=solver.E[ixt, iyt, :, 'z'] 
+        hf2['#'+str(n).zfill(5)]=solver.J[ixt, iyt, :, 'z'] 
 
 # Close save file
-hf.close()
-hf2.close()
+if save:
+    hf.close()
+    hf2.close()
 
 # Plot 3D built-in
-#solver.plot3D(field='H', component='y', clim=None,  hide_solids=None, show_solids=None, 
-#               add_stl='Solid 1', stl_opacity=0.1, stl_colors='white',
-#               title=None, cmap='rainbow', clip_volume=False, clip_normal='-y',
-#               clip_box=True, clip_bounds=None, off_screen=True, zoom=0.4, n=n)
+'''
+solver.plot3D(field='E', component='y', clim=None,  hide_solids=None, show_solids=None, 
+               add_stl='Cavity', stl_opacity=0.1, stl_colors='white', nan_opacity=1.0,
+               title=None, cmap='rainbow', clip_volume=False, clip_normal='-y',
+               clip_box=True, clip_bounds=None, off_screen=False, zoom=1.0, n=n)
+'''
+
+solver.plot3D(field='E', component='y', clim=None, add_stl='Cavity', stl_opacity=0.0, 
+               stl_colors='white', field_on_stl=True, nan_opacity=1.0, field_opacity=1.0,
+               cmap='rainbow', off_screen=False, zoom=1.0, n=n)
