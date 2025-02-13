ed24b39d4473c8ba761d7bb38ba1748a73595447
elenafuengar <elena.delafuenteg@gmail.com>
2025-02-13 18:48:15 +0100

    test: add reflection calculation and parametric scan

--- tests/_test_005_pml_boundaries.py
+++ tests/_test_005_pml_boundaries.py
@@ -7,52 +7,76 @@ from tqdm import tqdm
 sys.path.append('../')
 import wakis
 
-print("\n---------- Initializing simulation ------------------")
-# Domain bounds and grid
-xmin, xmax = -1., 1.
-ymin, ymax = -1., 1.
-zmin, zmax = 0., 10.
+n_pml_arr = [5,10,15,20,25,30]
+lo_pml_arr = [1e-5,5e-5,]#1.e-4,5.e-4,1.e-3,5.e-3,1.e-2,5.e-2,1.e-1]
 
-Nx, Ny = 20, 20
-Nz = 200
+#for n_pml in n_pml_arr:
+for lo_pml in lo_pml_arr:
 
-grid = wakis.GridFIT3D(xmin, xmax, ymin, ymax, zmin, zmax, 
-                       Nx, Ny, Nz)
+    print("\n---------- Initializing simulation ------------------")
+    # Domain bounds and grid
+    xmin, xmax = -1., 1.
+    ymin, ymax = -1., 1.
+    zmin, zmax = 0., 10.
 
-# Source
-amplitude = 1000.
-nodes = 15
-planeWave = wakis.sources.PlaneWave(xs=slice(0, Nx), ys=slice(0,Ny), zs=0, 
-                                    nodes=nodes, beta=1.0, amplitude=amplitude)
+    Nx, Ny = 20, 20
+    Nz = 200
+
+    grid = wakis.GridFIT3D(xmin, xmax, ymin, ymax, zmin, zmax, 
+                        Nx, Ny, Nz)
+
+    # Source
+    amplitude = 100.
+    nodes = 15
+    planeWave = wakis.sources.PlaneWave(xs=slice(0, Nx), ys=slice(0,Ny), zs=0, 
+                                        nodes=nodes, beta=1.0, amplitude=amplitude)
 
 
-# Boundary conditions and solver
-bc_low = ['periodic', 'periodic', 'pec']
-bc_high = ['periodic', 'periodic', 'pml']
-n_pml = 10 # number of pml cells
+    # Boundary conditions and solver
+    bc_low = ['pec', 'pec', 'pec']
+    bc_high = ['pec', 'pec', 'pml']
 
-solver = wakis.SolverFIT3D(grid, use_stl=False, 
-                           bc_low=bc_low, bc_high=bc_high,
-                           n_pml=n_pml)
-# Simulation
-Nt = int(2.0*(zmax-zmin)/c/solver.dt)
 
-for n in tqdm(range(Nt)):
+    solver = wakis.SolverFIT3D(grid, use_stl=False, 
+                            bc_low=bc_low, bc_high=bc_high,
+                            n_pml=10)
 
-    if n < Nt/4: # Fill half the domain
-        planeWave.update(solver, n*solver.dt)
+    solver.pml_lo = lo_pml #np.sqrt(1.e-4)
+    solver.pml_hi = np.sqrt(1.e-1)
+    solver.pml_func = np.geomspace
+    solver.fill_pml_sigmas()
+    solver.update_tensors('sigma')
 
-    solver.one_step()
+    # Simulation
+    Nt = int(2.0*(zmax-zmin)/c/solver.dt)
 
-    if n == 1000:
-        Hi = solver.H.copy()
-        Ei = solver.E.copy()
+    for n in tqdm(range(Nt)):
 
-    if n%200 == 0:
-        solver.plot2D('Hy', plane='ZY', pos=0.5, cmap='rainbow', 
-            interpolation='spline36', n=n, vmin=-amplitude, vmax=amplitude,
-            off_screen=True, title='005_img/2Dplot_Hy') 
-        
-    if n == 4000:
-        Hf = solver.H.copy()
-        Ef = solver.E.copy()
+        if n < Nt/4: # Fill half the domain
+            planeWave.update(solver, n*solver.dt)
+
+        solver.one_step()
+
+        if False: #n == 1000:
+            Hi = solver.H.copy()
+            Ei = solver.E.copy()
+
+        if n%3500 == 0 and n !=0:
+            solver.plot2D('Hy', plane='ZY', pos=0.5, cmap='bwr', 
+                interpolation='spline36', n=n, vmin=-amplitude, vmax=amplitude,
+                off_screen=True, title=f'005_img/Hy_lin_n{solver.n_pml}_{solver.pml_lo:.1e}') 
+                    
+        if n == 3500:
+            Hf = solver.H.copy()
+            Ef = solver.E.copy()
+
+    reflection = Hf.get_abs()[:,:,20:].max()
+    results = np.array([[solver.n_pml, 
+                        f"{solver.pml_lo:.1e}", 
+                        f"{solver.pml_hi:.1e}",
+                        solver.pml_func.__name__, 
+                        reflection]], 
+                        dtype=object)
+
+    with open('005_results.txt', "a") as f:
+        np.savetxt(f, results, fmt="%s", delimiter=" ")
