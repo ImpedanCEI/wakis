b6c52a584922e7ffab42931aa353e8b85b58f937
elenafuengar <elena.delafuenteg@gmail.com>
2025-02-25 12:21:43 +0100

    feature: Functions to extract from `.STP` files `solid` names, `colors`, and `materials`: `wakis.geometry.extract_XXX(stp_file)` to easily build the input dictionaries needed for `GridFIT3D`

--- wakis/geometry.py
+++ wakis/geometry.py
@@ -6,6 +6,82 @@
 import numpy as np
 import re
 
+def extract_colors_from_stp(stp_file):
+    lines = np.genfromtxt(stp_file, dtype=str, delimiter="\n", encoding="utf-8", comments=None)
+
+    solids = []
+    colors = []
+    color_dict = {}
+
+    # Regex patterns
+    solid_pattern = re.compile(r"#(\d+)=MANIFOLD_SOLID_BREP\('([^']+)'.*;")
+    color_pattern = re.compile(r"#(\d+)=COLOUR_RGB\('?',([\d.]+),([\d.]+),([\d.]+)\);")
+
+    # Extract colors
+    for line in lines:
+        color_match = color_pattern.search(line)
+        if color_match:
+            r, g, b = float(color_match.group(2)), float(color_match.group(3)), float(color_match.group(4))
+            colors.append([r, g, b])
+
+    # Extract solids
+    for line in lines:
+        solid_match = solid_pattern.search(line)
+        if solid_match:
+            solid_name = solid_match.group(2)
+            solids.append(solid_name)
+
+    for i in range(len(solids)):
+        color_dict[solids[i]] = colors[i]
+
+    return color_dict
+
+def extract_materials_from_stp(stp_file):
+    lines = np.genfromtxt(stp_file, dtype=str, delimiter="\n", encoding="utf-8", comments=None)
+
+    solid_dict = {}
+    material_dict = {}
+
+    # Regex patterns
+    solid_pattern = re.compile(r"#(\d+)=MANIFOLD_SOLID_BREP\('([^']+)'.*;")
+    material_pattern = re.compile(r"#(\d+)=PRESENTATION_LAYER_ASSIGNMENT\('([^']+)','[^']+',\(#([\d#,]+)\)\);")
+
+    # Extract solids
+    for line in lines:
+        solid_match = solid_pattern.search(line)
+        if solid_match:
+            solid_number = int(solid_match.group(1))
+            solid_name = solid_match.group(2)
+            solid_dict[solid_number] = solid_name
+
+    # Extract materials
+    for line in lines:
+        material_match = material_pattern.search(line)
+        if material_match:
+            material_name = material_match.group(2)
+            solid_numbers = [int(num.strip("#")) for num in material_match.group(3).split(',')] # Extract numbers as a list
+            
+            for solid_number in solid_numbers:
+                if solid_number in solid_dict:  # Only assign if it's a known solid
+                    material_dict[solid_dict[solid_number]] = material_name
+
+    return material_dict
+
+def extract_solids_from_stp(stp_file):
+    solids, materials = extract_names_from_stp(stp_file)
+    stl_solids = {}
+    for i in range(len(list(solids.keys()))):
+        solid = solids[list(solids.keys())[i]]
+        mat = materials[list(solids.keys())[i]]
+
+        # Remove problematic characters
+        solid_re = re.sub(r'[^a-zA-Z0-9_-]', '-', solid)
+        mat_re = re.sub(r'[^a-zA-Z0-9_-]', '-', mat)
+        name = f'{str(i).zfill(3)}_{solid_re}_{mat_re}'
+        stl_solids[solid] = name + '.stl'
+
+    return stl_solids
+
 def extract_names_from_stp(stp_file):
     """
     Extracts solid names and their corresponding materials from a STEP (.stp) file.
@@ -59,8 +135,7 @@ def extract_names_from_stp(stp_file):
 
     return solid_dict, material_dict
 
-
-def extract_stl_solids_from_stp(stp_file):
+def generate_stl_solids_from_stp(stp_file):
     """
     Extracts solid objects from a STEP (.stp) file and exports them as STL files.
 
@@ -94,21 +169,12 @@ def extract_stl_solids_from_stp(stp_file):
                         ''')
     
     stp = cq.importers.importStep(stp_file)
-    solids, materials = extract_names_from_stp(stp_file)
+    solid_dict = extract_solids_from_stp(stp_file)
 
     print(f"Generating stl from file: {stp_file}... ")
-    stl_solids = {}
     for i, obj in enumerate(stp.objects[0]):
-        solid = solids[list(solids.keys())[i]]
-        mat = materials[list(solids.keys())[i]]
-
-        # Remove problematic characters
-        solid = re.sub(r'[^a-zA-Z0-9_-]', '-', solid)
-        mat = re.sub(r'[^a-zA-Z0-9_-]', '-', mat)
-        name = f'{str(i).zfill(3)}_{solid}_{mat}'
-
+        name = solid_dict[list(solid_dict.keys())[i]]
         print(name+".stl")
-        stl_solids[name] = name + '.stl'
-        obj.exportStl(f'{str(i).zfill(3)}_{solid}_{mat}'+".stl") 
+        obj.exportStl(name+".stl") 
 
-    return stl_solids
+    return solid_dict
