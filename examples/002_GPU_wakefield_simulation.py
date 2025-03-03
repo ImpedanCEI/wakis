6a3f6e88c307151000427f7360d2525ea1f47ae7
elenafuengar <elena.delafuenteg@gmail.com>
2025-03-03 18:59:35 +0100

    style: add copy header

--- examples/002_GPU_wakefield_simulation.py
+++ examples/002_GPU_wakefield_simulation.py
@@ -1,3 +1,8 @@
+# copyright ################################# #
+# This file is part of the wakis Package.     #
+# Copyright (c) CERN, 2024.                   #
+# ########################################### #
+
 import os
 import numpy as np
 import pyvista as pv
